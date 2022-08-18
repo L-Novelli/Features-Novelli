c8f03d5f28572dfb944b6501f4a396aa85bc43ab
Lautaro <l.novelli@outlook.com>
2022-08-18 10:41:21 -0300

    sign up error ??

---
+++ constants/DataBase.js
@@ -0,0 +1,5 @@
+export const URL_API = 'https://coder1-a833c-default-rtdb.firebaseio.com/'
+
+export const URL_AUTH_SIGNUP = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBHgKsCTTDVnw5A1NmD1RGE_5nalC6LlLQ'
+
+// export const API_MAPS_KEY = 'AIzaSyDUKsRM2XVCCMDMAOkEuP5VX5Cm2aY99Ps'
